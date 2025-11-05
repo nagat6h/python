dbcf30b4ac4d9ead117ae018dbe73f664df67a28
nagat6h <nagats3do@gmail.com>
2025-11-05 18:15:34 +0300

    اضافة ملف تكاليف للبايثون

---
+++ python/Assignment.py
@@ -0,0 +1,28 @@
+# تكليف من درس 1 الى 10
+# تكليف الاول
+#قم بإنشاء ملف جديد لتبدأ كتابة الكود فيه ثم في أعلى الملف قم بكتابة Multiple Line Comment توصف فيه الملف ويمكنك كتابة ما تريد, لا توجد مشكلة
+#---------
+#التكليف 02
+#قم بإنشاء ثلاث متغيرات تحتوي على إسمك وسنك وبلدك, ويكون نوعهم String
+name = "Najat"
+age = "38"
+country = "Eritrea"
+#قم بطباعة القيم الموجودة في المتغيرات السابقة في ثلاث أسطر تحت بعضهم وقبلهم اسم يعبر عن المتغيرات المطلوب طباعة ما في ال Output التالي بنفس الشكل\
+#"Name: Osama"
+#"Age: 38"
+#"Country: Egypt"
+print("Name: " + name)
+print("Age: " + age)
+print("Country: " + country)
+#التكليف 04
+#قم بعمل Concatenate للمتغيرات مع بعض الكلمات لتخرج بهذه الرسالة الموجودة في ال Output التالي
+#"Hello, My Name Is Osama And Iam 38 Years Old and I Live in Egypt."
+print("Hello, My Name Is " + name + " And Iam " + age + " Years Old and I Live in " + country + ".")
+#التكليف  5
+#قم بطباعة نوع كل متغير من المتغيرات التي أنشأناها في سطر منفصل ليظهر لنا ال Output التالي
+#<class 'str'>
+#<class 'str'>
+#<class 'str'>
+print(type(name))
+print(type(age))
+print(type(country))
